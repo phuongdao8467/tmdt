a521ae0369efbd639e9567d29e06be5f36c5397e
phuongdao8467 <drac19021501bb@gmail.com>
2021-10-18 00:38:50 +0700

    fix payreturn

--- test.php
+++ test.php
@@ -16,4 +16,9 @@
 	else{
 		echo "Thành công";
 	}
+	$user_id = 4;
+	$counpon_id = NULL;
+	$result=mysqli_query($conn,"select num from usercoupon where user_id='$user_id'and coupon_id='$counpon_id';");
+    $check=mysqli_fetch_array($result);
+	echo gettype($check);
 ?>
